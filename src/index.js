80a355933b6f54d57afe58183d0b68928eebbc02
Ikejrmillsowoo <pooppymills2@gmail.com>
2021-06-26 22:49:24 -0400

    inital sample react page

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import List from './containers/List'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+    return (
+        <div className="container-fluid" >
+           <nav className="navbar sticky-top navbar-light bd-dark" >
+               <h1 className="navbar-brand text-light">MovieList</h1>
+           </nav>
+
+            <List />
+        </div>
+    )
+}
+
+ReactDOM.render(
+<App />, 
+document.getElementById('root')
+    )
+
